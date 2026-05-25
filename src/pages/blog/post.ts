0f215a8ea24a7736e6f275f55854cc2917492c4c
Monique Simberg <150544229+monsimb@users.noreply.github.com>
2026-05-25 15:59:27 -0400

    Add ViewEncapsulation and tags to BlogPostPage

--- src/pages/blog/post.ts
+++ src/pages/blog/post.ts
@@ -1,4 +1,4 @@
-import { Component, signal, inject, OnInit } from '@angular/core';
+import { Component, signal, inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogService } from './blog.services';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,8 @@ import DOMPurify from 'dompurify';
   selector: 'blog-post-page',
   imports: [CommonModule, HttpClientModule],
   templateUrl: './post.html',
-  styleUrl: './post.scss'
+  styleUrl: './post.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class BlogPostPage implements OnInit {
   private route = inject(ActivatedRoute);
@@ -23,21 +24,39 @@ export class BlogPostPage implements OnInit {
   html = signal<SafeHtml | string>('');
   title = signal<string>('');
   date = signal<string>('');
+  tags = signal<string[]>([]);
 
   ngOnInit() {
     const slug = this.route.snapshot.paramMap.get('slug');
-    if (slug) {
-      this.blog.getPostMd(slug).subscribe(md => {
-        this.content.set(md);
-        const dirty = marked.parse(md);
-        const clean = DOMPurify.sanitize(dirty);
-        this.html.set(this.sanitizer.bypassSecurityTrustHtml(clean));
-      });
 
-      this.blog.getPosts().subscribe(posts => {
-        const p = posts.find(x => x.slug === slug);
-        if (p) { this.title.set(p.title); this.date.set(p.date); }
-      });
+    if (!slug) {
+      this.title.set('Post not found');
+      return;
     }
+
+    this.blog.getPostMd(slug).subscribe(md => {
+      const body = this.stripFrontMatter(md);
+
+      this.content.set(body);
+
+      const dirty = marked.parse(body) as string;
+      const clean = DOMPurify.sanitize(dirty);
+
+      this.html.set(this.sanitizer.bypassSecurityTrustHtml(clean));
+    });
+
+    this.blog.getPosts().subscribe(posts => {
+      const p = posts.find(x => x.slug === slug);
+
+      if (p) {
+        this.title.set(p.title);
+        this.date.set(p.date);
+        this.tags.set(p.tags ?? []);
+      }
+    });
+  }
+
+  private stripFrontMatter(md: string): string {
+    return md.replace(/^---[\s\S]*?---\s*/, '');
   }
 }
